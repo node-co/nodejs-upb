af382946716ee537bc9c99e4901ae53c69e61524
camilo <camilomontoya2@gmail.com>
2018-12-06 23:57:24 -0500

    removiendo archivo ejemplos

--- ejemplos.js
+++
@@ -1,7 +0,0 @@
-console.log(typeof 'hola soy un string :)');
-
-let miArray = [0, 1, 2, 3, 4, 5];
-console.log('miArray1', miArray);
-const eliminado = miArray.splice(3, 1);
-console.log('miArray2', miArray);
-console.log('eliminado', eliminado);
